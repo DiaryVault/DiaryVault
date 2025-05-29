bcbdb26296a004fc73610cc7128259c16d11dc77
Stephen Lee <jungmindl@gmail.com>
2025-05-29 16:34:40 -0700

    base.html header fix

--- core/settings.py
+++ core/settings.py
@@ -141,8 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/dashboard/'
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 ACCOUNT_EMAIL_REQUIRED = True
