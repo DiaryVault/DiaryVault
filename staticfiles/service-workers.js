a10cf11d63c19ff712425026b4178c97c7cdd67f
Stephen Lee <jungmindl@gmail.com>
2025-05-29 14:54:17 -0700

    404 505 fix

---
+++ staticfiles/service-workers.js
@@ -0,0 +1,29 @@
+// DiaryVault Service Worker
+const CACHE_NAME = 'diaryvault-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/main.css',
+  '/static/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
