c8d67454aa3fcf6253624b4b038c47c88da8a5b7
Stephen Lee <jungmindl@gmail.com>
2025-06-07 15:04:46 +0900

    import decorators revised

--- diary/views/api.py
+++ diary/views/api.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 from django.utils import timezone
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST, csrf_exempt
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.db.models import Count, Avg
